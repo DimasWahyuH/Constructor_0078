164a83799357bb5e5faa2ba2c550dd1cb75f678d
DimasWahyuH <dimaswh19@gmail.com>
2024-05-13 15:12:25 +0700

    Menambahkan fungsi mahasiswa setID

--- Member statik function/Member statik function.cpp
+++ Member statik function/Member statik function.cpp
@@ -6,5 +6,25 @@ class mahasiswa
 private :
 	static int nim;
 
+public:
+	int id;
+	string nama;
 
+	void setID();
+	void printAll();
+
+	static void setNim(int pnim) { nim = pnim; }
+	static int getNim() { return nim; }
+
+	mahasiswa(string pnama): nama(pnama)
+	{
+		setID();
+	}
 };
+
+int mahasiswa::nim = 100;
+
+void mahasiswa::setID()
+{
+	id = ++nim;
+}
